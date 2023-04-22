38f455851a29092681d64e2d4150a15dec048b1c
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-22 14:39:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle"
+ * return 0.
+ * */
+
+int main (void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+
+}
