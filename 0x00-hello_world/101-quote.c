0198658dcc3159913f6c9f89b87c99c5d860242e
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-23 00:02:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - prints ("and that piece of art is useful\" -Dora Korpar, 2015-10-19,");
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 55);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 55);
 	return (1);
 }
