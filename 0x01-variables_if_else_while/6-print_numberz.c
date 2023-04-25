1aa471e8a47b64a2b8291d57438941991bea47bb
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-25 11:21:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabetically
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+		return (0);
+}
