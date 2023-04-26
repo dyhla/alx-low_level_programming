db73da42967a680aa1fe1813199275f031126fa9
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-26 10:54:34 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet
+ * Returns void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c == 'a'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
