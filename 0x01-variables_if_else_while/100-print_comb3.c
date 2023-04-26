4972de21a7e24bea01c1d8c64e5e413dfe19d8fd
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-26 03:49:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,14 @@ int main(void)
 		{
 			putchar(a + '0');
 			putchar(b + '0');
-			if (a != 8 || b != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			putchar('\n');
 		}
-			return (0);
+		if (a != 8 || b != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
 	}
 }
+
