1505a34f1f61b874cfdadc1949df1f8d84ce4802
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-27 05:27:41 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * test functions if prints positive_or_negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	1 = 0;
+	positive_or_negative(1);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#indef MAIN.h
+#define MAIN.h
+
+# include <stdio.h>
+
+void positive_or_negative(int 1);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
