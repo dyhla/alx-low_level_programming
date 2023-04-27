f778d99e3da25b2375567721c215a780e00dad07
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-27 04:18:16 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	int x, y, z;
+
 	for (x = 0; x <= 7; x++)
 	{
 		for (y = x + 1; y <= 8; y++)
 		{
 			for (z = y + 1; z <= 9; z++)
 			{
-				if ((x != y) !=z)
+				if ((x != y) != z)
 				{
 					putchar(x + '0');
 					putchar(y + '0');
