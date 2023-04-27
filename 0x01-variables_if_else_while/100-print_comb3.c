c06dbafb3c40f1aca2848c1608cc3348ebad3afe
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-27 01:51:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,20 @@
 int main(void)
 {
 
-	int y;
+	int i, j;
 
-	for (y = 48; y <= 57; y++)
+	for (i = 0; i <= 8; i++)
 	{
-		putchar(',');
-		putchar(' ');
+		for (j = i + 1; j <= 9; j++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i !=8 || j !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	}
 	putchar('\n');
 	return (0);
