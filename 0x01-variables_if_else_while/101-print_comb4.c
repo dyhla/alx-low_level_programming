78d6c6084bb9a95014ef86cb56f5e7a4410cecb5
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-27 04:13:45 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /*
  *  main - entry point
- *  Description: prints combination of numbers
+ *  Description: puts unique combination of numbers
  *  Return: always 0
  */
 
 int main(void)
 {
-	int a, b;
-
-	for (a  = '0'; a  < '9'; a++)
+	int x, y, z;
+	for (x = 0; x <= 7; x++)
 	{
-		for (b = a + 1; b  <= '9'; b++)
+		for (y = x + 1; y <= 8; y++)
 		{
-			putchar(a + '0');
-			putchar(b + '0');
-			if (a !=8 || b !=9)
+			for (z = y + 1; z <= 9; z++)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
+				if ((x != y) !=z)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(z + '0');
+					if (x == 7 && y == 8)
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
