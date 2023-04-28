10a3a194e52473ece1a40a4e0541fb8fcc9790d4
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-04-28 00:38:33 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - Entry point for the program
- * test functions if prints positive_or_negative
+ * main - tests functions that prints ifinteger is positive_or_negative
  * Return: 0
+ *
  */
 
 int main(void)
 {
 	int i;
 
-	i = 10;
-	positive_or_negative(i);
+	i = 0;
 
+	positive_or_negative(i);
 	return (0);
 }
