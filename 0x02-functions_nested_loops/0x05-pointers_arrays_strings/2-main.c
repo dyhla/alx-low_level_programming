b82721bf04f56b580b1016115303b408a4fcbc5a
lydiahobare98@gmail.com <lydiahobare98@gmail.com>
2023-05-02 01:16:12 -0700

    2-strlen.c

---
+++ 0x02-functions_nested_loops/0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
